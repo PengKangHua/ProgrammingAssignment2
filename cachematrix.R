e65429209d7d87963770af9953bfdcb661644e56
Peng Kang Hua <PengkangHua.taiwan@gmail.com>
2015-04-26 18:53:12 +0800

    change vector to matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ##
+    solvematrix <- NULL
+    set <- function(y) {
+        x <<- y
+        solvematrix << NULL
+    }
+    
+    get <- function() x
+    setsolvematrix <- function(s_meatri) sm <<- s_meatri
+    getsolvematrix <- function() m
+    
+    list(set = set, get = get,
+    setmatrix = setmatrix,
+    getsolvematrix = getmatrix)
 }
 
 
@@ -12,4 +25,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolvematrix()
+        if(!if.null(m)){
+            message("getting cached data")
+            return(m)
+        }
+        
+        data <- x$get()
+        m <- solve(m,...)
+        x$setsolvematrix(m)
+        m
 }
